9ee8b352380795590cf1412041b93e8f22d3a583
Santhosh-p653 <santhosh11042007@gmail.com>
2025-08-13 00:19:22 +0530

    traingle added successfully

---
+++ triangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class triangle{
+	public static void main(String []args){
+	int n=1;
+	while(n<=7){
+	int m=1; 
+	while(m<=n){
+	System.out.print(n);m=m+1;
+	}
+	System.out.println();n++;
+	}
+}
+}
